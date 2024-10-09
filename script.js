6a079a28e7eabb079ca348a74517c0ef162de3ae
Pep <1679807+jllaneras@users.noreply.github.com>
2024-10-10 00:19:07 +0200

    Allow switching to spotify provider

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-var tracks = [
-  {
-    "song": "Into The Black",
-    "tidal": "131334629",
-    "spotify": "2A6kF8nb0GuwxrMMrKsvtJ"
+function isAndroid() {
+  return /Android/i.test(navigator.userAgent);
+}
+
+class Track {
+  constructor(tidalId, spotifyId, title) {
+    this.providers = {
+      tidal: {
+         browser: "https://listen.tidal.com/track/" + tidalId,
+	 app: "tidal://track/" + tidalId
+      },
+      spotify: {
+	 browser: "https://open.spotify.com/track/" + spotifyId,
+	 app: "spotify:track:" + spotifyId + ":play"
+      }
+    };
+    this.title = title;
   }
-];
-
-
-function isMobileDevice() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  getUrl(provider, platform) {
+    return this.providers[provider][platform];
+  }
 }
 
-function play() {
-    // var url = "https://tidal.com/browse/track/"
-    var url = "https://open.spotify.com/track/{id}"
-    if (isMobileDevice()) {
-        // url = "tidal://track/"
-	url = "spotify:track:{id}:play"
-    }
-    // var track = tracks[0]["tidal"];
-    var id = tracks[0]["spotify"];
-    url = url.replace("{id}", id);
+class Playlist {
+  constructor(tracks) {
+    this.tracks = tracks;
+  }
+  randomTrack() {
+    return this.tracks[Math.floor(Math.random()*this.tracks.length)];
+  }
+  play() {
+    let query = window.location.search.substring(1);
+    let provider = (query == "spotify") ? "spotify" : "tidal";
+    let platform = isAndroid() ? "app" : "browser";
+    let url = this.randomTrack().getUrl(provider, platform);
     window.open(url, "_blank").focus();
+  }
 }
 
+const playlist = new Playlist([
+  new Track("131334629", "2A6kF8nb0GuwxrMMrKsvtJ", "Into the Black"),
+  new Track("90410954", "53oc5U2cz4MydANM35abYV", "Darkhorse"),
+  new Track("15427705", "6PGToxtt6KrZztJe6j66in", "Five Seconds"),
+]);
